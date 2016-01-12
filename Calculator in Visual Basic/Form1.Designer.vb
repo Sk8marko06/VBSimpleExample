b69bb80949bf7c64ada46673dfe49d58bc1b1b3d
Sk8marko06 <freestyle05@wp.pl>
2016-01-12 21:14:37 +0100

    init commit

---
+++ Calculator in Visual Basic/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.calculateBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.answerLbl = New System.Windows.Forms.Label()
+        Me.symbolLbl = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(146, 179)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox7.TabIndex = 4
+        Me.TextBox7.Text = "11.0"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(236, 179)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(80, 20)
+        Me.TextBox8.TabIndex = 5
+        Me.TextBox8.Text = "5.0"
+        '
+        'calculateBtn
+        '
+        Me.calculateBtn.Location = New System.Drawing.Point(13, 250)
+        Me.calculateBtn.Name = "calculateBtn"
+        Me.calculateBtn.Size = New System.Drawing.Size(75, 23)
+        Me.calculateBtn.TabIndex = 6
+        Me.calculateBtn.Text = "Sprawdz"
+        Me.calculateBtn.UseVisualStyleBackColor = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(13, 221)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(75, 23)
+        Me.clearBtn.TabIndex = 7
+        Me.clearBtn.Text = "Wyczysc"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'answerLbl
+        '
+        Me.answerLbl.AutoSize = True
+        Me.answerLbl.Location = New System.Drawing.Point(101, 255)
+        Me.answerLbl.Name = "answerLbl"
+        Me.answerLbl.Size = New System.Drawing.Size(0, 13)
+        Me.answerLbl.TabIndex = 8
+        '
+        'symbolLbl
+        '
+        Me.symbolLbl.AutoSize = True
+        Me.symbolLbl.Location = New System.Drawing.Point(138, 189)
+        Me.symbolLbl.Name = "symbolLbl"
+        Me.symbolLbl.Size = New System.Drawing.Size(0, 13)
+        Me.symbolLbl.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Pierwszy odcinek:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Drugi odcinek:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Pierwszy punkt (x,y) :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Drugi punkt (x,y) :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(144, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox1.TabIndex = 15
+        Me.TextBox1.Text = "-10.0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(234, 42)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(82, 20)
+        Me.TextBox2.TabIndex = 14
+        Me.TextBox2.Text = "-4.0"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(144, 68)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox3.TabIndex = 21
+        Me.TextBox3.Text = "-7.0"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(236, 68)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(80, 20)
+        Me.TextBox4.TabIndex = 20
+        Me.TextBox4.Text = "-2.0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(33, 182)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 13)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "Drugi punkt (x,y) :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(33, 156)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(106, 13)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "Pierwszy punkt (x,y) :"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(146, 153)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox5.TabIndex = 17
+        Me.TextBox5.Text = "5.0"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(236, 153)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(80, 20)
+        Me.TextBox6.TabIndex = 16
+        Me.TextBox6.Text = "1.0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(341, 297)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.symbolLbl)
+        Me.Controls.Add(Me.answerLbl)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.calculateBtn)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents calculateBtn As System.Windows.Forms.Button
+    Friend WithEvents clearBtn As System.Windows.Forms.Button
+    Friend WithEvents answerLbl As System.Windows.Forms.Label
+    Friend WithEvents symbolLbl As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Calculator in Visual Basic/Form1.vb
@@ -0,0 +1,68 @@
+﻿'This tutorial is provided in part by Server Intellect Web Hosting Solutions _http://www.serverintellect.com
+' Visit _http://v4.vbasic.net for more v4.vbasic.net Tutorials
+
+Public Class Form1
+
+    Private Sub calculateBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles calculateBtn.Click
+        Dim a_x As Double
+        Dim a_y As Double
+        Dim b_x As Double
+        Dim b_y As Double
+
+        Dim c_x As Double
+        Dim c_y As Double
+        Dim d_x As Double
+        Dim d_y As Double
+
+        Dim ab_x As Double
+        Dim ab_y As Double
+        Dim cd_x As Double
+        Dim cd_y As Double
+
+        Dim answer As Double
+
+        a_x = Double.Parse(TextBox1.Text)
+        a_y = Double.Parse(TextBox2.Text)
+        b_x = Double.Parse(TextBox3.Text)
+        b_y = Double.Parse(TextBox4.Text)
+
+        c_x = Double.Parse(TextBox5.Text)
+        c_y = Double.Parse(TextBox6.Text)
+        d_x = Double.Parse(TextBox7.Text)
+        d_y = Double.Parse(TextBox8.Text)
+
+        ' Warunek sprawdzający czy wektory są równoległe:
+        ' http://www.matematyka.pl/128191.htm
+        ' http://zadane.pl/zadanie/369584
+
+        ab_x = b_x - a_x
+        ab_y = b_y - a_y
+
+        cd_x = d_x - c_x
+        cd_y = d_y - c_y
+
+        answer = (ab_x * cd_y) - (ab_y * cd_x)
+
+        If answer = 0 Then
+            answerLbl.Text = "Odcinki są równoległe"
+        Else
+            answerLbl.Text = "Odcinki nie są równoległe"
+        End If
+
+    End Sub
+
+    Private Sub clearBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles clearBtn.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+
+        answerLbl.Text = "Result"
+
+    End Sub
+
+End Class
